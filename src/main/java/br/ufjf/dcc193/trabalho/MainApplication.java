f8da7d59b83b2cb521e0ff5fb31782eeb07bb404
Rian das Dores Alves <rianalves2009.ra@gmail.com>
2019-04-18 10:39:42 -0300

    Commit Inicial - Criação do Projeto

---
+++ src/main/java/br/ufjf/dcc193/trabalho/MainApplication.java
@@ -0,0 +1,14 @@
+package br.ufjf.dcc193.trabalho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+		System.out.println("Primeiro Trabalho de DCC193 - 2019.1");
+	}
+
+}
