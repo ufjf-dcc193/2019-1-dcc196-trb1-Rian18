5dafcdb411fa20e49d8e579e9ee688004bf8e6f4
Rian das Dores Alves <rianalves2009.ra@gmail.com>
2019-04-18 16:06:49 -0300

    Inserção de Annotations

---
+++ src/main/java/br/ufjf/dcc193/trabalho/Sede.java
@@ -0,0 +1,89 @@
+package br.ufjf.dcc193.trabalho;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Sede {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String nomefantasia;
+    private String endereco, bairro, cidade;
+    private String telefone;
+    private String endweb;
+
+    public Sede()
+    {
+
+    }
+    public Sede(String nomeFantasia,String endereco,String bairro, String cidade, String telefone, String endweb)
+    {
+        this.nomefantasia = nomeFantasia;
+        this.endereco = endereco;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.telefone = telefone;
+        this.endweb = endweb;
+    }
+
+   
+    public Long getId() {
+        return id;
+    }
+    public String getNomefantasia() {
+        return nomefantasia;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEndweb() {
+        return endweb;
+    }
+
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public void setNomefantasia(String nomefantasia) {
+        this.nomefantasia = nomefantasia;
+    }
+    
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+    
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+    
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+    public void setEndweb(String endweb) {
+        this.endweb = endweb;
+    }
+
+}
