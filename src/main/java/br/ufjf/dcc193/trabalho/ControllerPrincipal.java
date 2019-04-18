402c624abbb8a48143f1445d88bbc0c1684afd87
Rian das Dores Alves <rianalves2009.ra@gmail.com>
2019-04-18 14:22:19 -0300

    ModelAndView /alteraçes no application. properties

--- src/main/java/br/ufjf/dcc193/trabalho/ControllerPrincipal.java
+++ src/main/java/br/ufjf/dcc193/trabalho/ControllerPrincipal.java
@@ -1,16 +1,21 @@
 
- package br.ufjf.dcc193.trabalho;
+package br.ufjf.dcc193.trabalho;
+
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
 public class ControllerPrincipal {
-@RequestMapping("index.html")
-public String inicial()
-{
-    return "inicio.jsp";
-}
+    @RequestMapping("index.html")
+    public ModelAndView inicial ()
+    {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("inicio");
+        return mv;
+    }
+
     
 }
