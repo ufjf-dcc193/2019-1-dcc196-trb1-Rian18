374258dfcccd1ff2cc2c398ca068f54b49be5980
Rian das Dores Alves <rianalves2009.ra@gmail.com>
2019-04-24 02:04:16 -0300

    Criação das Classes repository

--- src/main/java/br/ufjf/dcc193/trabalho/model/Atividade.java
+++ src/main/java/br/ufjf/dcc193/trabalho/model/Atividade.java
@@ -4,7 +4,6 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Entity
 public class Atividade {
