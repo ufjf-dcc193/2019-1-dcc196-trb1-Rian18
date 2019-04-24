5052f2ad68edbcf00478973f62e3e04fe5a37e6d
Rian das Dores Alves <rianalves2009.ra@gmail.com>
2019-04-24 01:57:30 -0300

    Alteração na pasta model

--- src/main/java/br/ufjf/dcc193/trabalho/model/Membro.java
+++ src/main/java/br/ufjf/dcc193/trabalho/model/Membro.java
@@ -4,8 +4,6 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
